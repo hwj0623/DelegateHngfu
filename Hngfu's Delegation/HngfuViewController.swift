db952cc25f3b81014d9845b98a62c535f7ec6f36
Hngfu <djsl1018@naver.com>
2019-06-23 08:37:29 +0900

    [HngfuTableView] UITableViewDataSource, Delegate 채택, 준수

--- Hngfu's Delegation/HngfuViewController.swift
+++ Hngfu's Delegation/HngfuViewController.swift
@@ -14,11 +14,36 @@ class HngfuViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.hngfuTableView.dataSource = self
+        self.hngfuTableView.delegate = self
     }
     
     @IBAction func tapCloseButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension HngfuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonsInfo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hngfu", for: indexPath)
+        guard let hngfuCell = cell as? HngfuTableViewCell else { return cell }
+        hngfuCell.show(with: pokemonsInfo[indexPath.row])
+        return hngfuCell
+    }
+}
+
+extension HngfuViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let name = pokemonsInfo[indexPath.row].name
+        player.play(with: name)
     }
 }
 
