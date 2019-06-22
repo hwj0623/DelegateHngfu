87d7044fed5619ec4c0c09260e51d050e27ba817
Hngfu <djsl1018@naver.com>
2019-06-22 20:12:25 +0900

    프로젝트 생성 및 초기 세팅

---
+++ Hngfu's Delegation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hngfu's Delegation
+//
+//  Created by 조재흥 on 19. 6. 22..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var unoTextField: UITextField!
+    @IBOutlet weak var dueTextField: UITextField!
+    @IBOutlet weak var treTextField: UITextField!
+    @IBOutlet weak var quattroTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
